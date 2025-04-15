f5cc7d9c0aa8bacafb1ecee8cba475584f9b5243
lizagqq <100711776+lizagqq@users.noreply.github.com>
2025-04-16 00:18:43 +0300

    changed the host to connect to the database

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',       //  пользователь базы данных
-    host: 'localhost',      // Адрес сервера базы данных
+   connectionString: process.env.DATABASE_URL,     // Адрес сервера базы данных
     database: 'travel_planner', // Имя базы данных
     password: '1111',  // Пароль пользователя
     port: 5432,             // Порт подключения (по умолчанию 5432)
